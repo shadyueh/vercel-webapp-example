df966474bf0c04345e5e11ae816dc9e61ac54cee
Held Grijo <held.grijo@gmail.com>
2020-11-11 11:46:06 -0400

    Problem in not use useState

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+function Contador(params) {
+    let contador = 5;
+
+    function adicionarContador() {
+        contador = contador + 1;
+        console.log(contador);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
 function Home(){
-    return <div>Home</div>
+    return <Contador/>
 }
 
 export default Home
